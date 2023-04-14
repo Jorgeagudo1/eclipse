e97f890d8d8581db78ddb573e0f5577ad7cb21d3
alumnoTARDE <jorge.agusan@educa.jcyl.es>
2023-04-14 20:27:38 +0200

    validando mi segundo commit

---
+++ src/HolaMundo/HolaMundo.java
@@ -0,0 +1,17 @@
+package HolaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("hola mi primer mundo");
+		System.out.println("hola mi segundo mundo");
+		System.out.println("hola mi tercer cambia1 mundo");
+		System.out.println("hola mi tercer cambia2 mundo");
+		System.out.println("hola mi tercer cambia3 mundo");
+		System.out.println("hola mi tercer cambia4 mundo");
+	
+		
+
+	}
+
+}
